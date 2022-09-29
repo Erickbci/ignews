ecbc6fac049d238998a0c61f13bf8027ea91ff44
Erickbci <batista.erick07@hotmail.com>
2022-09-28 22:13:51 -0300

    Componente: SignInButton

---
+++ src/Components/SignInButton/index.tsx
@@ -0,0 +1,24 @@
+import styles from './styles.module.scss'
+import { FaGithub } from 'react-icons/fa'
+import { FiX } from 'react-icons/fi'
+
+export function SignInButton() {
+    const isUserLoggedIn = true
+
+    return isUserLoggedIn ? (
+        <button 
+        className={styles.signInButton}
+        type='button'>
+            <FaGithub color='#04d361' />
+            Erick Batista
+            <FiX color='#737380' className={styles.closeIcon}/>
+        </button>
+    ) : (
+        <button 
+        className={styles.signInButton}
+        type='button'>
+            <FaGithub color='#eba417' />
+            Sign In With GitHub
+        </button>
+    )
+}
